f22a4be3e264aca52e0f90dcbdfe2506a2058e81
Emre Akbas <emreakbas@gmail.com>
2018-03-01 10:47:31 +0300

    first fully functional version

--- mathgen.py
+++ mathgen.py
@@ -8,17 +8,24 @@ Created on 25. 7. 2012.
 import urllib
 import re
 import sys
+import random
+import time
+import sys
 from graphviz import Digraph
 
-prefix = "http://genealogy.math.ndsu.nodak.edu/id.php?id="
-prefix = "http://www.genealogy.ams.org/id.php?id="
+# to prevent character encoding errors
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+prefixes = ["http://genealogy.math.ndsu.nodak.edu/id.php?id=",
+        "http://www.genealogy.ams.org/id.php?id=", 
+        "http://genealogy.math.uni-bielefeld.de/id.php?id="]
 cache = {}
 
-dot = Digraph(comment='X''s academic genealogy')
+dot = Digraph(node_attr={'shape': 'box'})
 
 def get_page(url):
     try:
-        print url
         return urllib.urlopen(url).read()
     except:
         return ""
@@ -47,18 +54,16 @@ def get_advisors_string(advisors):
     
 def crawl(math_id):
     if math_id not in cache:
-        page = get_page(prefix + math_id)
+        page = get_page(random.choice(prefixes) + math_id)
         name = remove_excess_space(get_name(page))
         year = remove_excess_space(get_year(page))
+        print(name, year)
         dot.node(str(math_id), name+"\n"+year)
         cache[math_id] = name+"\n"+year
-        #advisors_string = get_advisors_string([crawl(advisor_id) for advisor_id in get_advisor_ids(page)])
-        #dot.render()
-        import time
-        time.sleep(2)
 
-        for advisor_id in get_advisor_ids(page):
-            print(advisor_id)
+        # let's be nice to the MathGenealogy servers and wait at least 1 second
+        # between queries
+        time.sleep(1)
 
         for advisor_id in get_advisor_ids(page):
             dot.edge(str(advisor_id), str(math_id))
@@ -69,4 +74,10 @@ def crawl(math_id):
     
 if __name__ == '__main__':
     math_id = sys.argv[1] if len(sys.argv) >= 2 else ""
-    print crawl(math_id)
+    crawl(math_id)
+
+    # save output
+    dot.save(math_id+"-tree.dot")
+
+    # produce pdf
+    dot.render()
